7bd24e1e5ee7f23e2a8ea9cc44723070e9bdeddf
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-23 13:49:11 -0400

    credit card finished

--- Pset1/creditCard.c
+++ Pset1/creditCard.c
@@ -2,61 +2,134 @@
 #include <cs50.h>
 #include <math.h>
 
-int lengthCheck(long cardNum){
+int lengthCheck(long cardNum)
+{
     int counter = 0;
-    while(cardNum>0){
-        cardNum = cardNum/10;
-        counter+=1;
-    }; 
+    while (cardNum > 0)
+    {
+        cardNum = cardNum / 10;
+        counter += 1;
+    };
     return counter;
 };
 
-//to get individual digits do num %10^digit place needed
-string cardName(long cardNum, int length){
-    int firstDigit = cardNum % pow(10, length);
-    int secondDigit = cardNum % pow(10, length-1)
-    string name = ""
-    switch(length){
-        case 13:
-            if(firstDigit == 4){
-                name = "VISA";
-            }else{
-                name = "INVALID";
-            };
-            return name;
-        case 15:
-            if(firstDigit == 3){
-                if(secondDigit == 4 || secondDigit == 7){
-                    name = "AMEX";
-                }else{
-                 name = "INVALID";
-                };
-            }else{
+// To get individual digits do num %10^digit place needed
+string cardName(string name, long cardNum, int length)
+{
+    int length1 = length - 1;
+    int length2 = length - 2;
+    long firstPow = pow(10, length1);
+    long secondPow = pow(10, length2);
+    int firstDigit = cardNum / firstPow;
+    int secondDigit = cardNum / secondPow;
+    switch (length)
+    {
+    case 13:
+        if (firstDigit == 4)
+        {
+            name = "VISA";
+        }
+        else
+        {
             name = "INVALID";
-            };
-            return name;
-        case 16:
-            if(firstDigit == 4){
-                name = "VISA";
-            }else if(firstDigit == 4){
-                if(secondDigit <= 1 && secondDigit >= 5){
-                    name = "MASTERCARD"
-                }else{
+        };
+        return name;
+    case 15:
+        if (firstDigit == 3)
+        {
+            if (secondDigit == 34 || secondDigit == 37)
+            {
+                name = "AMEX";
+            }
+            else
+            {
                 name = "INVALID";
-                };
-            }else{
-                name = "INVALID";
             };
-            return name;
-        default:
+        }
+        else
+        {
             name = "INVALID";
-            return name;
+        };
+        return name;
+    case 16:
+        if (firstDigit == 4)
+        {
+            name = "VISA";
+        }
+        else if (firstDigit == 5)
+        {
+            if (secondDigit >= 51 && secondDigit <= 55)
+            {
+                name = "MASTERCARD";
+            }
+            else
+            {
+                name = "INVALID";
+            };
+        }
+        else
+        {
+            name = "INVALID";
+        };
+        return name;
+    default:
+        name = "INVALID";
+        return name;
     };
 };
 
-int main(void){
+string testNum(long cardNum, int length, string name)
+{
+    int total = 0;
+    int lengthCopy = length;
+    char arr[lengthCopy];
+    while (lengthCopy--)
+    {
+        arr[lengthCopy] = cardNum % 10;
+        cardNum /= 10;
+    };
+    int count = 1;
+    for (int i = length - 1; i >= 0; i--)
+    {
+        if (count % 2 == 0)
+        {
+            int numDoubled = arr[i] * 2;
+            if (numDoubled / 10 != 0)
+            {
+                int splitNumSum = (numDoubled % 10) + 1;
+                total += splitNumSum;
+            }
+            else
+            {
+                total += numDoubled;
+            }
+        }
+        else
+        {
+            total += arr[i];
+        };
+        count++;
+    };
+    if (total % 10 != 0)
+    {
+        name = ("INVALID");
+    };
+    return name;
+};
+
+int main(void)
+{
     long cardNum = get_long("Please enter a valid credit car number: ");
     int length = lengthCheck(cardNum);
-    string cardName =  cardName(cardNum, length);
-    printf("%ld\n%d\n%s\n",cardNum, length, cardName);
-}
+    string name = "";
+    name = cardName(name, cardNum, length);
+    string invalid = "INVALID";
+    if (name == invalid)
+    {
+        printf("%s\n", name);
+    }
+    else
+    {
+        printf("%s\n", testNum(cardNum, length, name));
+    };
+};
--- Pset1/mario.c
+++ Pset1/mario.c
@@ -3,31 +3,35 @@
 
 void print(int n, int y)
 {
-        string hashtags = "#";
-        if(n > 1)
-        {
-           print(n-1, y+1);
-        };
-         for(int i = y; i > 0; i--){
-             printf(" ");
-         }
-         for(int i = 1; i <= n; i++){
-             printf("%s",hashtags);
-         }
-        printf("  ");
-         for(int i = 1; i <= n; i++){
-             printf("%s", hashtags);
-         }
-        printf("\n");
-}
+    string hashtags = "#";
+    if (n > 1)
+    {
+        print(n - 1, y + 1);
+    };
+    for (int i = y; i > 0; i--)
+    {
+        printf(" ");
+    };
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%s", hashtags);
+    };
+    printf("  ");
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%s", hashtags);
+    };
+    printf("\n");
+};
 
-int main(void){
+int main(void)
+{
     int x = -1;
     int zero = 0;
     x = get_int("Please input a number from 1 to 8: ");
-    if(x < 1 || x > 8){
+    if (x < 1 || x > 8)
+    {
         return main();
-    }
-    //recursively print #'s
+    };
     print(x, zero);
-}
+};
