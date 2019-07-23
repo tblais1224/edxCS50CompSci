10e87039007f61ee5bcf50bbe2bfe66c7db6d4e0
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-22 20:44:16 -0400

    hello and mario pset1 done

---
+++ Pset1/mario.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print(int n, int y)
+{
+        string hashtags = "#";
+        if(n > 1)
+        {
+           print(n-1, y+1);
+        };
+         for(int i = y; i > 0; i--){
+             printf(" ");
+         }
+         for(int i = 1; i <= n; i++){
+             printf("%s",hashtags);
+         }
+        printf("  ");
+         for(int i = 1; i <= n; i++){
+             printf("%s", hashtags);
+         }
+        printf("\n");
+}
+
+int main(void){
+    int x = -1;
+    int zero = 0;
+    x = get_int("Please input a number from 1 to 8: ");
+    if(x < 1 || x > 8){
+        return main();
+    }
+    //recursively print #'s
+    print(x, zero);
+}
