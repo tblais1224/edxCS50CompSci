2a1e780eb5e8888e1ba0e3fd1283406056933559
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-27 17:43:40 -0400

    trying to match character to alphabet array chars

--- Pset2/ceaser.c
+++ Pset2/ceaser.c
@@ -4,23 +4,54 @@
 
 int main(int argc, string argv[])
 {
-    string alphabet[26] = {"a", "b", "c", "d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"};
-    string alphabetCaps[26] = {"A", "B", "C", "D", "E", "F","G", "H", "I","J", "K", "L","M", "N", "O","P", "Q", "R","S", "T", "U", "V", "W", "X", "Y", "Z"};
+    char *alphabet[26] = {"a", "b", "c", "d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"};
+    char *alphabetCaps[26] = {"A", "B", "C", "D", "E", "F","G", "H", "I","J", "K", "L","M", "N", "O","P", "Q", "R","S", "T", "U", "V", "W", "X", "Y", "Z"};
     
     int key = 0;
     string plainText = "";
     key = get_int("Please input a number greater than 0: ");
     plainText = get_string("Please enter a string to be encrypted: ");
-    printf("%d    %s\n", key, plainText);
-    
-    char s[100];
-    int c = 0;
-    printf("\n Please Enter your Full Name: \n");
-    scanf("%s", s);
- 
-    while (s[c] != '\0') {
-       printf("%c", s[c]);
-       c++;
+    int count = 0;
+    while(plainText[count])
+    {
+        char character[2] = "\0";
+        character[0] = plainText[count];
+        char *newCharacter = character;
+        for(int i = 0; i < 26; i++)
+        {
+            char *alphChar = alphabet[i];
+            char *alphCharCap = alphabetCaps[i];
+            if(newCharacter == alphChar)
+            {
+                printf("hi");
+                int newIndex = i + key;
+                if(newIndex < 26)
+                {
+                    newCharacter = alphabet[newIndex];
+                }
+                else
+                {
+                    int newerIndex = newIndex - 26;
+                    newCharacter = alphabet[newIndex];
+                }
+                i = 26;
+            }
+            else if(character == alphCharCap)
+            {
+                int newIndex = i + key;
+                if(newIndex < 26)
+                {
+                    newCharacter = alphabetCaps[newIndex];
+                }
+                else
+                {
+                    int newerIndex = newIndex - 26;
+                    newCharacter = alphabetCaps[newIndex];
+                };
+                i = 26;
+            };
+        };
+        printf("%s" , newCharacter);
+        count++;
     }
-
 }
