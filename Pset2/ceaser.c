bd6a505a66e598c3597294b0f3edda9495e8039d
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-26 22:29:52 -0400

    trying to print character in loop with multi word string

--- Pset2/ceaser.c
+++ Pset2/ceaser.c
@@ -2,18 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void splitInput(string plainText)
-{
-    string chars;
-    chars=strtok(plainText, "");
-    printf("hi   %s\n", chars);
-    while (chars!= NULL)
-    {
-        printf ("im working     %s\n",chars);
-        chars = strtok (NULL, ",:");
-    }
-}
-
 int main(int argc, string argv[])
 {
     string alphabet[26] = {"a", "b", "c", "d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"};
@@ -24,5 +12,15 @@ int main(int argc, string argv[])
     key = get_int("Please input a number greater than 0: ");
     plainText = get_string("Please enter a string to be encrypted: ");
     printf("%d    %s\n", key, plainText);
-    splitInput(plainText);
+    
+    char s[100];
+    int c = 0;
+    printf("\n Please Enter your Full Name: \n");
+    scanf("%s", s);
+ 
+    while (s[c] != '\0') {
+       printf("%c", s[c]);
+       c++;
+    }
+
 }
