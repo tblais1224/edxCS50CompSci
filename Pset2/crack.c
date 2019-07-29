0f567a08daf618e00093c32ebd6284816228be00
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-29 00:02:27 -0400

    use brute force to check all combos of letters or words from some library

--- Pset2/crack.c
+++ Pset2/crack.c
@@ -1,7 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <crypt.h>
+
+
+// 50cI2vYkF0YU2   =  LOL
+
+char *getSalt(char * hashPass)
+{
+    static char salt[3] = "\0";
+    salt[0] = hashPass[0];
+    salt[1] = hashPass[1];
+    return salt;
+}
 
 int main(int argc, string argv[])
 {
-
+    char * hashedPass;
+    if(argc == 2)
+    {
+        hashedPass = argv[1];
+        printf("%s\n", hashedPass);
+    }
+    else
+    {
+        return 1;
+    }
+    char * salt = getSalt(hashedPass);
+    char * test = crypt("LOL", salt);
+    printf("%s   %s\n",salt, test);
 }
