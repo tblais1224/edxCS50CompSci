1b99d1139dd7757635f35541318c52189b0990db
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-09 21:36:38 -0400

    trying to fix substrings

--- pset7/similarities/similarities/helpers.py
+++ pset7/similarities/similarities/helpers.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import sent_tokenize
 
+
 def lines(a, b):
     outputSet = set()
     aLines = a.split("\n")
@@ -12,14 +13,36 @@ def lines(a, b):
 
 
 def sentences(a, b):
-    """Return sentences in both a and b"""
-
+    outputSet = set()
+    aSentences = sent_tokenize(a)
+    bSentences = sent_tokenize(b)
+    for x in aSentences:
+        if x in bSentences:
+            outputSet.add(x)
     # TODO
-    return []
+    return list(outputSet)
 
 
 def substrings(a, b, n):
-    """Return substrings of length n in both a and b"""
+    outputSet = set()
+    aSubstrings = []
+    bSubstrings = []
 
+    for i in range(0, len(a), n):
+        string = ""
+        for k in range(0, n):
+            if len(a) > i + k:
+                string = string + a[i + k]
+        aSubstrings.append(string)
+    for i in range(0, len(b), n):
+        string = ""
+        for k in range(0, n):
+            if len(b) > i + k:
+                string = string + b[i + k]
+        bSubstrings.append(string)
+
+    for x in aSubstrings:
+        if x in bSubstrings:
+            outputSet.add(x)
     # TODO
-    return []
+    return list(outputSet)
